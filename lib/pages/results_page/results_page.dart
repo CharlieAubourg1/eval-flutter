c7d22d40a26ee3afb5b8a47eb15af2be018260c3
CharlieAubourg1 <charlie.aubourg@gmail.com>
2026-04-17 10:10:06 +0200

    arrange results page

--- lib/pages/results_page/results_page.dart
+++ lib/pages/results_page/results_page.dart
@@ -12,124 +12,34 @@ class ResultsPage extends StatelessWidget {
     AppLocalizations localizations = AppLocalizations.of(context)!;
     return Scaffold(
       appBar: GreenTrackAppBar(),
-      body: Column(
-        children: [
-          Text(
-            localizations.section_results,
-            style: TextStyle(
-              fontSize: 26.0,
-              fontWeight: FontWeight.w900,
-              color: AppColors.primaryDark,
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              localizations.section_results,
+              style: const TextStyle(
+                fontSize: 26.0,
+                fontWeight: FontWeight.w900,
+                color: AppColors.primaryDark,
+              ),
             ),
-          ),
-          ScoreWidget(value: 15.1),
-        ],
+            const SizedBox(height: 24),
+
+            Center(
+              child: SizedBox(
+                width: 180,
+                height: 180,
+                child: ScoreWidget(value: 0.4),
+              ),
+            ),
+
+            const SizedBox(height: 32),
+            // TODO
+          ],
+        ),
       ),
     );
   }
 }
-
-// enum ScoreLevel { excellent, bon, mediocre, mauvais, execrable }
-
-// ScoreLevel _getScoreLevel(double value) {
-//   if (value < 1.0) return ScoreLevel.excellent;
-//   if (value < 3.0) return ScoreLevel.bon;
-//   if (value < 5.0) return ScoreLevel.mediocre;
-//   if (value < 8.0) return ScoreLevel.mauvais;
-//   return ScoreLevel.execrable;
-// }
-
-// class _ResultIcon extends StatelessWidget {
-//   const _ResultIcon({required this.value});
-
-//   final double value;
-
-//   ScoreLevel get _level {
-//     if (value < 1.0) return ScoreLevel.excellent;
-//     if (value < 3.0) return ScoreLevel.bon;
-//     if (value < 5.0) return ScoreLevel.mediocre;
-//     if (value < 8.0) return ScoreLevel.mauvais;
-//     return ScoreLevel.execrable;
-//   }
-
-//   Color get _backgroundColor {
-//     switch (_level) {
-//       case ScoreLevel.excellent:
-//         return AppColors.scoreExcellentBackground;
-//       case ScoreLevel.bon:
-//         return AppColors.scoreGoodBackground;
-//       case ScoreLevel.mediocre:
-//         return AppColors.scoreFairBackground;
-//       case ScoreLevel.mauvais:
-//         return AppColors.scorePoorBackground;
-//       case ScoreLevel.execrable:
-//         return AppColors.scoreVeryPoorBackground;
-//     }
-//   }
-
-//   String _getLabel(AppLocalizations localizations) {
-//     switch (_level) {
-//       case ScoreLevel.excellent:
-//         return localizations.score_label_excellent;
-//       case ScoreLevel.bon:
-//         return localizations.score_label_good;
-//       case ScoreLevel.mediocre:
-//         return localizations.score_label_fair;
-//       case ScoreLevel.mauvais:
-//         return localizations.score_label_poor;
-//       case ScoreLevel.execrable:
-//         return localizations.score_label_very_poor;
-//     }
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     AppLocalizations localizations = AppLocalizations.of(context)!;
-//     return Container(
-//       width: 160,
-//       height: 160,
-//       decoration: BoxDecoration(
-//         color: _backgroundColor,
-//         borderRadius: BorderRadius.circular(48),
-//       ),
-//       child: Column(
-//         mainAxisAlignment: MainAxisAlignment.center,
-//         children: [
-//           Text(
-//             value.toString(),
-//             style: const TextStyle(
-//               fontSize: 50,
-//               fontWeight: FontWeight.w900,
-//               color: AppColors.primaryDark,
-//               height: 1.0,
-//             ),
-//           ),
-//           Text(
-//             localizations.score_unit,
-//             style: const TextStyle(
-//               fontSize: 12,
-//               color: AppColors.primary,
-//               fontWeight: FontWeight.w900,
-//             ),
-//           ),
-//           const SizedBox(height: 12),
-//           Container(
-//             padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 6),
-//             decoration: BoxDecoration(
-//               color: AppColors.white,
-//               borderRadius: BorderRadius.circular(20),
-//             ),
-//             child: Text(
-//               _getLabel(localizations),
-//               style: const TextStyle(
-//                 fontSize: 13,
-//                 fontWeight: FontWeight.w500,
-//                 color: AppColors.primary,
-//               ),
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
